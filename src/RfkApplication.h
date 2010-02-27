fd54ea1936a62d65ceb90e3a9a6626301fd27fa7
Thomas Thurman <tthurman@gnome.org>
2010-02-27 12:13:31 -0500

    commenting

--- src/RfkApplication.h
+++ src/RfkApplication.h
@@ -6,19 +6,42 @@
 #include "RfkController.h"
 #include "RfkWindow.h"
 
+/**
+ * The game itself.
+ */
 class RfkApplication: public QApplication {
 
   Q_OBJECT
 
  public:
+  /**
+   * Constructor.
+   *
+   * \param argc  The number of arguments on the commandline.
+   * \param argv  The arguments on the commandline.
+   */
   RfkApplication(int &argc, char **argv);
 
   public slots:
+  /**
+   * Throws away the existing model and creates a new one.
+   */
   void recreateModel();
 
  private:
+  /**
+   * The model of the current board.
+   */
   RfkBoardModel *m_model;
+
+  /**
+   * The controller of the game as a whole.
+   */
   RfkController *m_controller;
+
+  /**
+   * The main window, which includes the view.
+   */
   RfkWindow *m_window;
 };
 
