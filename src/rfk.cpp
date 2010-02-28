14573e38498632a4eb988e3c13caec6566ceece6
Thomas Thurman <tthurman@gnome.org>
2010-02-28 18:22:10 -0500

    rm todo

--- src/rfk.cpp
+++ src/rfk.cpp
@@ -8,7 +8,6 @@
  * This is a generic Qt port, but it is intended to be used
  * with Maemo on the Nokia N900 (and later on, MeeGo).
  *
- * \todo Add "running" capability to the controller.
  * \todo Add chequers to the view showing where to click.
  * \todo Add the online help.
  * \todo Add random movement.
